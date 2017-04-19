395bd51a7734c0da1a24d283eb4345ea8443f3e3
billywithbelly <billychen1216@gmail.com>
2017-04-19 23:15:44 +0800

    decode data

--- mqtt.js
+++ mqtt.js
@@ -1,7 +1,17 @@
+function hexToDec(hex) {
+    var result = 0, digitValue;
+    hex = hex.toLowerCase();
+    for (var i = 0; i < hex.length; i++) {
+        digitValue = '0123456789abcdefgh'.indexOf(hex[i]);
+        result = result * 16 + digitValue;
+    }
+    return result;
+}
+
 var mqtt = require('mqtt');
 var opt = {
 	port:1883,
-	clientID: '1908158da9f54d71be7793477ff4371f'
+	clientID: '4184eb5d0e8942bb83408c61bdf17af9'
 
 };
 
@@ -15,7 +25,34 @@ client.on('message', function (topic, msg){
 	var result = msg.toString();
 	result = result.substring(1, result.length-1);
 	var obj = JSON.parse(result);
+	
 	if (obj.macAddr == '0000000000000396'){
 		console.log('recieved:  macAddr:'+obj.macAddr+' data:'+obj.data);
+		var parse = obj.data.toString ();
+		var lngth = parse.length;
+		for (var i=1; i<4; i++) {
+			switch (i) {
+				case 1: {
+					var tmpt = parse.substring (lngth-i*2, lngth-(i-1)*2);
+					console.log ('temperature : ' + 'hex : ' + parse.substring (lngth-i*2, lngth-(i-1)*2) + ' decimal : ' + hexToDec(tmpt));
+					break;
+				}
+				case 2: {
+					var tmpt = parse.substring (lngth-i*2, lngth-(i-1)*2);
+					console.log ('magnetic disturbance : ' + 'hex : ' + parse.substring (lngth-i*2, lngth-(i-1)*2) + ' decimal : ' + hexToDec(tmpt));
+					break;
+				}
+				case 3: {
+					var tmpt = parse.substring (lngth-(i+1)*2, lngth-(i-1)*2);
+					console.log ('temperature : ' + 'hex : ' + parse.substring (lngth-(i+1)*2, lngth-(i-1)*2) + ' decimal : ' + (hexToDec(tmpt) / 1000) );
+					break;
+				}
+				default : {
+					console.log ('nothing to log');
+					break;
+				}
+			}
+		}
 	}
+	
 });
