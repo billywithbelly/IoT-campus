fcc836ba08da872df83802eff21c0eb9763d305c
Safanar <satan251008@gmail.com>
2017-05-24 16:28:30 +0800

    Update socket.js

--- public/js/socket/socket.js
+++ public/js/socket/socket.js
@@ -8,21 +8,23 @@ export default class Socket{
 		});
 		this.socket.on('bikes',(response)=>{
 			var bikes = response.result;
-			var ourbike = {};
+
+			var ourbikes = [];
 			for(var i=0;i<bikes.length;i++){
 				var obj = new Object;
-				obj._id = bikes[i]._id;
+				obj.id = bikes[i]._id;
 			  obj.state = bikes[i].data.charAt(7);
 				obj.battery = bikes[i].data.charAt(3);
 				obj.location = {
 				latitude  : 24.795942,
 				longitude : 120.996966
-			        };
+			  };
 				obj.kid = null;
 				obj.time = bikes[i].time;
-				ourbike.push(obj);
+				ourbikes[i] = obj;
 			};
-			this.map.setBikes(ourbike);
+
+			this.map.setBikes(ourbikes);
 		});
 	}
 }
