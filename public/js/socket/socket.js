d178592ca280603502575590218feae6a0e5c8ff
Safanar <satan251008@gmail.com>
2017-06-15 07:16:45 +0800

    Add files via upload

--- public/js/socket/socket.js
+++ public/js/socket/socket.js
@@ -9,26 +9,13 @@ export default class Socket{
 		this.socket.on('bikes',(response)=>{
 			var bikes = response.result;
 
-			var ourbikes = [];
 			for(var i=0;i<bikes.length;i++){
-				var obj = new Object;
-				obj.id = bikes[i]._id;
-				if(bikes[i].data.charAt(8) == 0)obj.state = 0;
-				else obj.state = 1;
-				obj.battery = bikes[i].data.charAt(4);
-				obj.location = bikes[i].location;
-
-				//obj.kid = null;
-
-				d = new Date();
-				obj.time = new Date(new Date(bikes[i].time) - (d.getTimezoneOffset() * 60000));
-
-				console.log(bikes[i]);
-				console.log(obj);
-				ourbikes[i] = obj;
+				var d = new Date();
+				bikes[i].time = new Date(new Date(bikes[i].time) - (d.getTimezoneOffset() * 60000));
+				bikes[i].lasttime = new Date(new Date(bikes[i].lasttime));
 			};
 
-			this.map.setBikes(ourbikes);
+			this.map.setBikes(bikes);
 		});
 	}
 }
