23ac675a532a0c7bbfe3d08b82d0fa68d2ce7305
billywithbelly <billychen1216@gmail.com>
2017-09-10 22:57:10 +0800

    checking id

--- public/js/map/bike.js
+++ public/js/map/bike.js
@@ -10,23 +10,23 @@ export default class Bike{
       scaledSize: new google.maps.Size(10, 10), // scaled size
     };
 
-
+         
     if(bike.id == "0000000000000390" || bike.id == "0000000000000393" || bike.id == "0000000000000394" || bike.id == "0000000000000397" ||
-        bike.id == "000000000D01009B" || bike.id == "000000000D0100DE" || bike.id == "000000000D0100A1" || bike.id == "000000000D0100D6" 
-          || bike.id == "000000000D0100AC" || bike.id == "000000000000038F"){
+        bike.id == "000000000D01009B" || bike.id == "000000000D0100DE" || bike.id == "000000000D010041" || bike.id == "000000000D0100D6" 
+          || bike.id == "000000000D0100AC" || bike.id == "000000000000038F" || bike.id == "000000000D0100A1"){
         this.icon.url = 'http://icons.iconarchive.com/icons/custom-icon-design/flatastic-10/256/Trafficlight-green-icon.png';
       if(bike.state == "車位已滿"){
         this.icon.url = 'http://icons.iconarchive.com/icons/custom-icon-design/flatastic-10/256/Trafficlight-red-icon.png';
-        /*if(bike.lasttime != null){
-          if(bike.diff >= 30)this.icon.url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Yellow_Light_Icon.svg/232px-Yellow_Light_Icon.svg.png';
+        //if(bike.lasttime != null){
+          //if(bike.diff >= 30)this.icon.url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Yellow_Light_Icon.svg/232px-Yellow_Light_Icon.svg.png';
 
-        } else {
+        //} else {
           // create lasttime attribute ?
-        }
-        */
-
+        //}
+      
       }
     }
+    
 
 
     this.marker = new google.maps.Marker({
